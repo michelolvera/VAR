f0bf04ee5ebf9fd6934bafe977925712e7bbd899
Michel Olvera <michelolvera@icloud.com>
2018-11-08 10:29:13 -0600

    Nuevo: - Agregados los controladores restantes (Puede haber mas en un futuro)

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ArticulosReligiosos\Http\Controllers;
+
+use ArticulosReligiosos\Categorie;
+use Illuminate\Http\Request;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \ArticulosReligiosos\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \ArticulosReligiosos\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \ArticulosReligiosos\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \ArticulosReligiosos\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categorie $categorie)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SubcategorieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ArticulosReligiosos\Http\Controllers;
+
+use ArticulosReligiosos\Subcategorie;
+use Illuminate\Http\Request;
+
+class SubcategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \ArticulosReligiosos\Subcategorie  $subcategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subcategorie $subcategorie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \ArticulosReligiosos\Subcategorie  $subcategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subcategorie $subcategorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \ArticulosReligiosos\Subcategorie  $subcategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subcategorie $subcategorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \ArticulosReligiosos\Subcategorie  $subcategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subcategorie $subcategorie)
+    {
+        //
+    }
+}
